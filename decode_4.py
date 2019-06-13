83401fd79036059e5485c600120f124e2f770fc9
luicdayi <luicdawang0708@gmail.com>
2019-06-13 20:42:04 +0800

    color bug unfixed

---
+++ decode_4.py
@@ -0,0 +1,164 @@
+import argparse
+import random
+
+from PIL import Image
+
+def binary_decode(list_binary):
+ 
+  num=0
+ # print("in binary",list_binary)
+  for k in list_binary:
+    num = num + (1<<(k-1))
+  #  print(k,binary_1<<(k-1),num)
+  return num
+
+def  distinguish_layer(s1_pixels,s2_pixels,s3_pixels):
+    flag=[4,4,4]
+
+    for r in range (3):
+      if flag[0]==4:
+        tmp=s1_pixels[2,2][r]
+
+        for i in range(8):
+          if s1_pixels[i/3,i%3][r]!=tmp:
+            flag[0]=r
+            break
+        if flag[0]!=4:
+          continue
+
+      if flag[1]==4:
+        tmp=s2_pixels[2,2][r]
+
+        for i in range(8):
+          if s2_pixels[i/3,i%3][r]!=tmp:
+            flag[1]=r
+            break
+        if flag[1]!=4:
+          continue
+
+      if flag[2]==4:
+        tmp=s3_pixels[2,2][r]
+
+        for i in range(8):
+          if s3_pixels[i/3,i%3][r]!=tmp:
+            flag[2]=r
+            break
+        if flag[2]!=4:
+          continue
+
+   # print(flag)
+    if flag[0]>flag[1]:
+      s1_pixels,s2_pixels = s2_pixels,s1_pixels
+      flag[0],flag[1] = flag[1],flag[0]
+
+    if flag[1]>flag[2]:
+      s2_pixels,s3_pixels = s3_pixels,s2_pixels
+      flag[1],flag[2]=flag[2],flag[1]
+
+    if flag[0]>flag[1]:
+      s1_pixels,s2_pixels = s2_pixels,s1_pixels
+      flag[0],flag[1]=flag[1],flag[0] 
+
+#    print(flag)
+
+    return s1_pixels,s2_pixels,s3_pixels
+
+def decode(filename1, filename2,filename3):
+    """Generates a (2,2) visual cryptography scheme."""
+    first = Image.open(filename1)
+    s1_pixels = first.load()
+
+    #first.save("first.png")
+    
+    second = Image.open(filename2)
+    s2_pixels = second.load()
+
+    third = Image.open(filename3)
+    s3_pixels = third.load()
+    #second.save("second.png")
+    
+    decode = Image.new("RGB", (int(first.size[0]/3), int(first.size[1]/3)))
+    d_pixels = decode.load()
+
+    s1_pixels,s2_pixels,s3_pixels = distinguish_layer(s1_pixels,s2_pixels,s3_pixels)
+
+    for i in range(decode.size[0]):
+      for j in range(decode.size[1]):
+          # list_R
+          list1=[]
+          list2=[]
+          value=s1_pixels[i*3,j*3][0]
+          value2=0
+          for k in range(8):
+            if s1_pixels[i*3+k/3,j*3+k%3][0]==value:
+              list1.append(k+1)
+            else:
+              list2.append(k+1)
+              value2=s1_pixels[i*3+k/3,j*3+k%3][0]
+          
+          list_R=[]
+          if value-value2<0 or value-value2==2:
+            for x in list2:
+              list_R.append(x)
+          else:
+            for x in list1:
+              list_R.append(x)
+          
+          list1.clear()
+          list2.clear()
+          #print(list_R)
+
+          value=s2_pixels[i*3,j*3][1]
+          value2=0
+          for k in range(8):
+            if s2_pixels[i*3+k/3,j*3+k%3][1]==value:
+              list1.append(k+1)
+            else:
+              list2.append(k+1)
+              value2=s2_pixels[i*3+k/3,j*3+k%3][1]
+          list_G=[]
+          if value-value2<0 or value-value2==2:
+            for x in list2:
+              list_G.append(x)
+          else:
+            for x in list1:
+              list_G.append(x)
+
+          list1.clear()
+          list2.clear()
+         # print(list_G)
+
+          value=s3_pixels[i*3,j*3][2]
+          value2=0
+          for k in range(8):
+            if s3_pixels[i*3+k/3,j*3+k%3][2]==value:
+              list1.append(k+1)
+            else:
+              list2.append(k+1)
+              value2=s3_pixels[i*3+k/3,j*3+k%3][2]
+          list_B=[]
+          if value-value2<0 or value-value2==2:
+            for x in list2:
+              list_B.append(x)
+          else:
+            for x in list1:
+              list_B.append(x)
+          list1.clear()
+          list2.clear()
+          #print(list_B)
+          d_pixels[i,j]=(binary_decode(list_R),binary_decode(list_G),binary_decode(list_B))
+          #print(d_pixels[i,j])
+
+          #return
+            
+
+    decode.save("result_4.png")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("IMAGE1", help="The image to discrypt - 1")
+    parser.add_argument("IMAGE2", help="The image to discrypt - 2")
+    parser.add_argument("IMAGE3", help="The image to discrypt - 3")
+    args = parser.parse_args()
+
+    decode(args.IMAGE1, args.IMAGE2,args.IMAGE3)
--- encrypt_4.py
+++ encrypt_4.py
@@ -36,73 +36,94 @@ def setk(k):
 	if k == 9:
 		return 2, 2
 
-def encrypt(filename1,filename2,filename_s):
+def encrypt(filename1,filename2,filename3,filename_s):
+	phototype="RGB"
 	original_share1 = Image.open(filename1)
-	original_share1 = original_share1.convert("RGB")
+	original_share1 = original_share1.convert(phototype)
 	original_s1_pixels = original_share1.load()
 
 	original_share2 = Image.open(filename2)
-	original_share2 = original_share2.convert("RGB")
+	original_share2 = original_share2.convert(phototype)
 	original_s2_pixels = original_share2.load()
 
+	original_share3 = Image.open(filename3)
+	original_share3 = original_share3.convert(phototype)
+	original_s3_pixels = original_share3.load()
+
 	secret = Image.open(filename_s)
-	secret = secret.convert("RGB")
+	secret = secret.convert(phototype)
 	se_pixels = secret.load()
 
-	share1 = Image.new("RGB", (original_share1.size[0] * 3, original_share1.size[1] * 3))
+	length=original_share1.size[0]
+	width=original_share1.size[1]
+	share1 = Image.new(phototype, (length * 3, width * 3))
 	s1_pixels = share1.load()
-
-	share2 = Image.new("RGB", (original_share2.size[0] * 3, original_share2.size[1] * 3))
+	#s1_pixels[0,0]=(1,2,3,255)
+	#print(s1_pixels[0,0])
+	#return
+	share2 = Image.new(phototype, (length * 3, width * 3))
 	s2_pixels = share2.load()
 
-	for i in range(original_share1.size[0]):
-		for j in range(original_share1.size[1]):
-			
-			for r in range(3):
+	share3 = Image.new(phototype, (length * 3, width * 3))
+	s3_pixels = share3.load()
+
+	for i in range(length):
+		for j in range(width):
+			s1=[0,0,0]
+			s2=[0,0,0]
+			s3=[0,0,0]
+
+			for k in seq:
+				m,n=setk(k)
+				for r in range(3):
+
+					s=binary_convert(se_pixels[i,j][r])
 
 
-				s=binary_convert(se_pixels[i,j][r])
+					if k in s:
+						s1[r] = original_s1_pixels[i,j][r]
+						s2[r] = original_s2_pixels[i,j][r]
+						s3[r] = original_s3_pixels[i,j][r]
 
-				if len(s)< 4: 
-					bs=[x for x in seq if x not in s]
-				else:
-					bs=s
-				
-				bs1=get_random(bs, (int)(len(bs)/2))
-				bs2=[x for x in bs if x not in bs1]
+					elif r==0: ##not in s
+						s1[r] = (original_s1_pixels[i,j][r]-1)
+						if s1[r]<0:
+							s1[r]=2
+						s2[r] = original_s2_pixels[i,j][r]
+						s3[r] = original_s3_pixels[i,j][r]
 
-				if len(s)< 4: 
-					for x in s: ##and 
-						bs1.append(x)
-						bs2.append(x)
+					elif r==1:
+						s1[r] = original_s1_pixels[i,j][r]
+						s2[r] = (original_s2_pixels[i,j][r]-1)
+						if s2[r]<0:
+							s2[r]=2
+						s3[r] = original_s3_pixels[i,j][r]
 
+					elif r==2:
+						s1[r] = original_s1_pixels[i,j][r]
+						s2[r] = original_s2_pixels[i,j][r] 
+						s3[r] = (original_s3_pixels[i,j][r]-1)
+						if s3[r]<0:
+							s3[r]=2
 
-				#for k in seq:
+				s1_pixels[ i*3 + m , j*3 + n ]=(s1[0],s1[1],s1[2])
+				s2_pixels[ i*3 + m , j*3 + n ]=(s2[0],s2[1],s2[2])
+				s3_pixels[ i*3 + m , j*3 + n ]=(s3[0],s3[1],s3[2])
 
-					#m,n=setk(k)
-					#if (r==0):
-					#	s1_pixels[i * 3 + m, j * 3 + n] 
-					#	s2_pixels[i * 3 + m, j * 3 + n] 
-					#if k in bs1:
-					#	s1_pixels[i * 3 + m, j * 3 + n] + tuple(original_s1_pixels[i,j][r])
-					#else:
-					#	s1_pixels[i * 3 + m, j * 3 + n] + tuple(0)
-					#if k in bs2:
-					#	s2_pixels[i * 3 + m, j * 3 + n] + tuple(original_s2_pixels[i,j][r])
-					#else:
-					#	s2_pixels[i * 3 + m, j * 3 + n] + tuple(0)
 
 							
 			
-	share1.save("share1_RGBA.png")
-	share2.save("share2_RGBA.png")
+	share1.save("share1_RGB.png")
+	share2.save("share2_RGB.png")
+	share3.save("share3_RGB.png")
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("IMAGE_SHARE_1", help="The image to share 1")
     parser.add_argument("IMAGE_SHARE_2", help="The image to share 2")
+    parser.add_argument("IMAGE_SHARE_3", help="The image to share 3")
     parser.add_argument("IMAGE_SECRET", help="The image to hide")
     args = parser.parse_args()
 
-    encrypt(args.IMAGE_SHARE_1, args.IMAGE_SHARE_2, args.IMAGE_SECRET)
+    encrypt(args.IMAGE_SHARE_1, args.IMAGE_SHARE_2, args.IMAGE_SHARE_3, args.IMAGE_SECRET)
